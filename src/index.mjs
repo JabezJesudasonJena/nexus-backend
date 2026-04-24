1118663c581b3e6ddb8619ad79379227b58482c3
JabezJesudasonJena <boycalledjabez@gmail.com>
2026-04-24 13:51:32 +0530

    Added get request params

--- src/index.mjs
+++ src/index.mjs
@@ -3,22 +3,27 @@ import express from 'express';
 const app = express();
 const PORT = 5001;
 
-
-
-
-
-
+const mockUsers = [
+    {id: 1, userName: "john", displayName: "John"},
+    {id: 2, userName: "mark", displayName: "Mark"},
+    {id: 3, userName: "mahesh", displayName: "Mahesh"},
+]
 
 app.get("/", (req,res) => {
     res.send("Hello World !")
 })
 
+app.get("/api/users/:id", (req, res) => {
+    const parsedId = parseInt(req.params.id);
+    console.log(parsedId)
+    if(isNaN(parsedId)) return res.status(400).send({msg : 'Bad request'})
+    
+    const finduser = mockUsers.find((user)=> user.id === parsedId)
 
-
-
-
-
-
+    if(!finduser) return res.status(404).send({msg : "User Not found"});
+    
+    return res.send(finduser)
+})
 
 app.listen(PORT, () => console.log(`Server has started at http://localhost:${PORT}`))
 
