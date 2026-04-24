baf9f5b4b9ba9d7f4127c8d7a8713a4e496549d6
JabezJesudasonJena <boycalledjabez@gmail.com>
2026-04-24 20:44:32 +0530

    Added new request put

--- src/index.mjs
+++ src/index.mjs
@@ -56,6 +56,32 @@ app.post("/api/users", (req, res) => {
     // return res.status(200).send(req.body)
 })
 
+// Put request
+// It is used to update only a portion of the data
+app.put("/api/users/:id", (req, res) => {
+    // This will destructrize the req and extract body and id
+    const {body, params: {id}} = req;
+    // This will convert the id which is in string into int
+    const parsedId = Number.parseInt(id);
+    // This will check that if the id is valid 
+        // eg error 
+            // passing PUT /api/users/done or PUT /api/users/stringvalue
+    // This if will handle if any of the error above satisfies
+    if(!isNaN(parsedId)){
+        return res.sendStatus(400)
+    }
+    // this will return the index of the data v r going to change
+    const findUserIndex = mockUsers.findIndex(
+        (user) => user.id == parsedId
+    )
+    // To check weather the index is in the users database
+    if(findUserIndex == -1) return res.sendStatus(404); 
+    // Here the data in the users is been edited and returned as 204 refering of succcess edit of the data
+    mockUsers[findUserIndex] = {id : parsedId, ...body};
+    return res.sendStatus(204).json(mockUsers[findUserIndex]);
+})
+
+
 app.listen(PORT, () => console.log(`Server has started at http://localhost:${PORT}`))
 
 
