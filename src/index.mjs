096f33f69e6a146583ffd5f8fe59edaee85e88b1
JabezJesudasonJena <170162059+JabezJesudasonJena@users.noreply.github.com>
2026-04-25 17:27:27 +0530

    feat: session cookies

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,7 @@
 import express from "express";
 import routes from "./routes/index.mjs";
 import cookieParser from "cookie-parser";
+import session from "express-session";
 
 const app = express();
 const PORT = 5001;
@@ -10,6 +11,17 @@ app.use(express.json());
 // cookie-parser
 // The secret is passed inside refering the secret needed to parse the cookie
 app.use(cookieParser('123'))
+app.use(
+  session({
+    secret: 'jabezedu',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 6000 * 2,
+
+    }
+  })
+)
 /*
     Here the route file is used to use it v have to use .use function which
     means it will use the routes in the router -> ./routes/users.mjs
@@ -21,6 +33,7 @@ app.use(routes);
 
 // Default Route
 app.get("/", (req, res) => {
+  console.log(req.session)
   res.cookie('hello', 'world', {maxAge: 6000, signed: true});
   res.send("Hello World !");
 });
